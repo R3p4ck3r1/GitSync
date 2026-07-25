5853c4e7d19a4326837fbe2daa2dbb7fadd5d897
Th3-R3p4ck3r ✨ <ashrafonlinen.ow@gmail.com>
2026-07-25 21:41:19 +0300

    Fix return statement in _readPremiumStatus method

--- lib/api/manager/premium_manager.dart
+++ lib/api/manager/premium_manager.dart
@@ -17,7 +17,7 @@ class PremiumManager {
   }
 
   Future<bool> _readPremiumStatus() async {
-    return
+    return true ||
     // kDebugMode ||
     await repoManager.getBool(StorageKey.repoman_hasGHSponsorPremium);
   }
